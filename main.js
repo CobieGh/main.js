4267225db468bb88d1c75161bf775770d9d2a778
CobieGh <62403417+CobieGh@users.noreply.github.com>
2020-08-25 15:41:24 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 .header {
-  background-image: url('images/header-background.jpg');
+  background-image: url('https://websitesetup.org/wp-content/uploads/2019/01/bootstrap-tutorial-include-header-background-image-768x391.jpg');
   background-size: cover;
   background-position: center;
   position: relative;
