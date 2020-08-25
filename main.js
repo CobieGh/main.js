6d61c0ea088372063381127c4957b983aaa852cf
CobieGh <62403417+CobieGh@users.noreply.github.com>
2020-08-25 15:52:06 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-.header {
-  background-image: url('https://websitesetup.org/wp-content/uploads/2019/01/bootstrap-tutorial-include-header-background-image-768x391.jpg');
-  background-size: cover;
-  background-position: center;
-  position: relative;
-}
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
